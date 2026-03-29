1a53185ee7e91219cdc8a639d3c03efe9f390dc9
eltz-dev <eltzjb@gmail.com>
2026-03-29 16:18:02 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+function toggleMenu(){
+document.getElementById("sidebar").classList.toggle("active")
+}
+
+function showPage(page){
+document.getElementById("dashboard").style.display="none"
+document.getElementById("report").style.display="none"
+document.getElementById(page).style.display="block"
+}
+
+async function run(base,input,result){
+
+let value=document.getElementById(input).value
+let endpoint=base+encodeURIComponent(value)
+let url="/api/proxy?endpoint="+encodeURIComponent(endpoint)
+let box=document.getElementById(result)
+
+box.textContent="Loading..."
+
+try{
+
+let r=await fetch(url)
+let data=await r.json()
+
+box.textContent=JSON.stringify(data,null,2)
+
+}catch(e){
+
+box.textContent="Error : "+e
+
+}
+}
 
