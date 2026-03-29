fb1cd73878a54bf92d2aa35a38c17fd82e00a625
eltz-dev <eltzjb@gmail.com>
2026-03-29 16:20:03 +0700

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,1 +1,27 @@
+export default async function handler(req, res) {
+
+const { endpoint } = req.query
+
+if(!endpoint){
+return res.status(400).json({error:"endpoint required"})
+}
+
+try{
+
+const api = await fetch(endpoint)
+const data = await api.json()
+
+res.setHeader("Access-Control-Allow-Origin","*")
+
+res.status(200).json(data)
+
+}catch(e){
+
+res.status(500).json({
+error:"proxy error",
+message:e.toString()
+})
+
+}
+}
 
